82c83b21f1a95d5508b288e59f4a5f0ea9ba1839
Kneelawk <jedidiah.pommert@gmail.com>
2018-12-01 20:05:54 -0800

    Hide navigation bar

--- android/src/com/github/kneelawk/hellogdx/AndroidLauncher.java
+++ android/src/com/github/kneelawk/hellogdx/AndroidLauncher.java
@@ -1,16 +1,16 @@
 package com.github.kneelawk.hellogdx;
 
-import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.github.kneelawk.hellogdx.HelloGdx;
+
+import android.os.Bundle;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
 		initialize(new HelloGdx(), config);
 	}
 }
