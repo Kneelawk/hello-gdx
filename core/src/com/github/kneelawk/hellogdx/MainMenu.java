eaa4ab360f080d5cb2bfe1a4f3f70a1721dbb53b
Kneelawk <jedidiah.pommert@gmail.com>
2018-12-09 21:44:26 -0800

    Add resources for main menu

--- core/src/com/github/kneelawk/hellogdx/MainMenu.java
+++ core/src/com/github/kneelawk/hellogdx/MainMenu.java
@@ -1,21 +1,52 @@
 package com.github.kneelawk.hellogdx;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 public class MainMenu extends ScreenAdapter {
 
-	public MainMenu() {
+	private Stage stage;
 
+	public MainMenu() {
+		stage = new Stage();
+
+		Skin skin = new Skin();
+		skin.load(Gdx.files.absolute("default-k.json"));
+
+		Table table = new Table(skin);
+		table.setFillParent(true);
+		stage.addActor(table);
+	}
+
+	@Override
+	public void show() {
+		Gdx.input.setInputProcessor(stage);
+	}
+
+	@Override
+	public void hide() {
+		Gdx.input.setInputProcessor(null);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
 	}
 
 	@Override
 	public void render(float delta) {
-
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
 
 	@Override
 	public void dispose() {
-
+		stage.dispose();
 	}
 
 }
