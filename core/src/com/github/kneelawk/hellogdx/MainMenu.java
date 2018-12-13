9c3b95f7b0da906dd1e9af5318933f174afa8986
Kneelawk <jedidiah.pommert@gmail.com>
2018-12-13 13:20:43 -0800

    Fix main menu skin assets

--- core/src/com/github/kneelawk/hellogdx/MainMenu.java
+++ core/src/com/github/kneelawk/hellogdx/MainMenu.java
@@ -3,27 +3,32 @@ package com.github.kneelawk.hellogdx;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenu extends ScreenAdapter {
 
 	private Stage stage;
 
 	public MainMenu() {
-		stage = new Stage();
+		stage = new Stage(new ScreenViewport());
+
+		TextureAtlas atlas = new TextureAtlas(Gdx.files.absolute("skins/default-k/default-k.atlas"));
 
 		Skin skin = new Skin();
-		skin.load(Gdx.files.absolute("default-k.json"));
+		skin.addRegions(atlas);
+		skin.load(Gdx.files.absolute("skins/default-k/default-k.json"));
 
 		Table table = new Table(skin);
 		table.setFillParent(true);
 		stage.addActor(table);
-		
+
 		Image image = new Image(skin.getDrawable("hello-libgdx"));
-		table.add(image);
+		table.add(image).colspan(3);
 	}
 
 	@Override
